6436ce749844dc56ce07e5ab9f47b6e45950d950
LAPTOP-22GG1T1U\smarttoococo <smartpoppop@hotmail.com>
2018-08-04 09:15:33 +0700

    intitial laravel project

---
+++ app/Http/Controllers/DemoController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;//เรียกใช้ความสามารถของไรเบอร์รี้
+
+class DemoController extends Controller
+{
+    public function index()
+    {
+        return "Method GET: Index";
+    }
+
+    public function demotwo()
+    {
+        return "Method POST: demotwo";
+    }
+
+    public function demothree()
+    {
+        return "Method GET, POST : demothree";
+    }
+
+    public function demofour()
+    {
+        return "Method GET, POST, PUT/PATCH, DELETE : demofour";
+    }
+}
+ //
+
